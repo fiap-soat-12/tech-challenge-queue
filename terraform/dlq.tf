607745703bc36824ac641cf4b88888371f7dbcf3
Wilton Souza <wiltonsouzafilho@yahoo.com.br>
2025-01-28 20:59:05 -0300

    feat: Adding cook-order-create-queue and dlq on terraform config

--- terraform/dlq.tf
+++ terraform/dlq.tf
@@ -16,38 +16,20 @@ resource "aws_sqs_queue" "order-product-update-dlq" {
   max_message_size           = 262144
 }
 
-resource "aws_sqs_queue" "order-product-create-accept-dlq" {
-  name                       = "order-product-create-accept-dlq"
-  message_retention_seconds  = 1209600
-  max_message_size           = 262144
-}
-
-resource "aws_sqs_queue" "order-product-delete-accept-dlq" {
-  name                       = "order-product-delete-accept-dlq"
-  message_retention_seconds  = 1209600
-  max_message_size           = 262144
-}
-
-resource "aws_sqs_queue" "order-product-update-accept-dlq" {
-  name                       = "order-product-update-accept-dlq"
-  message_retention_seconds  = 1209600
-  max_message_size           = 262144
-}
-
 resource "aws_sqs_queue" "payment-order-create-dlq" {
   name                       = "payment-order-create-dlq"
   message_retention_seconds  = 1209600
   max_message_size           = 262144
 }
 
-resource "aws_sqs_queue" "payment-order-create-accept-dlq" {
-  name                       = "payment-order-create-accept-dlq"
+resource "aws_sqs_queue" "order-status-update-dlq" {
+  name                       = "order-status-update-dlq"
   message_retention_seconds  = 1209600
   max_message_size           = 262144
 }
 
-resource "aws_sqs_queue" "order-status-update-dlq" {
-  name                       = "order-status-update-dlq"
+resource "aws_sqs_queue" "cook-order-create-dlq" {
+  name                       = "cook-order-create-dlq"
   message_retention_seconds  = 1209600
   max_message_size           = 262144
 }
--- terraform/queue.tf
+++ terraform/queue.tf
@@ -46,54 +46,6 @@ resource "aws_sqs_queue" "order-product-update-queue" {
   depends_on = [aws_sqs_queue.order-product-update-dlq]
 }
 
-resource "aws_sqs_queue" "order-product-create-accept-queue" {
-  name                       = "order-product-create-accept-queue"
-  visibility_timeout_seconds = 30
-  message_retention_seconds  = 86400
-  delay_seconds              = 0
-  max_message_size           = 262144
-  receive_wait_time_seconds  = 0
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.order-product-create-accept-dlq.arn
-    maxReceiveCount     = 5
-  })
-
-  depends_on = [aws_sqs_queue.order-product-create-accept-dlq]
-}
-
-resource "aws_sqs_queue" "order-product-delete-accept-queue" {
-  name                       = "order-product-delete-accept-queue"
-  visibility_timeout_seconds = 30
-  message_retention_seconds  = 86400
-  delay_seconds              = 0
-  max_message_size           = 262144
-  receive_wait_time_seconds  = 0
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.order-product-delete-accept-dlq.arn
-    maxReceiveCount     = 5
-  })
-
-  depends_on = [aws_sqs_queue.order-product-delete-accept-dlq]
-}
-
-resource "aws_sqs_queue" "order-product-update-accept-queue" {
-  name                       = "order-product-update-accept-queue"
-  visibility_timeout_seconds = 30
-  message_retention_seconds  = 86400
-  delay_seconds              = 0
-  max_message_size           = 262144
-  receive_wait_time_seconds  = 0
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.order-product-update-accept-dlq.arn
-    maxReceiveCount     = 5
-  })
-
-  depends_on = [aws_sqs_queue.order-product-update-accept-dlq]
-}
-
 resource "aws_sqs_queue" "payment-order-create-queue" {
   name                       = "payment-order-create-queue"
   visibility_timeout_seconds = 30
@@ -110,22 +62,6 @@ resource "aws_sqs_queue" "payment-order-create-queue" {
   depends_on = [aws_sqs_queue.payment-order-create-dlq]
 }
 
-resource "aws_sqs_queue" "payment-order-create-accept-queue" {
-  name                       = "payment-order-create-accept-queue"
-  visibility_timeout_seconds = 30
-  message_retention_seconds  = 86400
-  delay_seconds              = 0
-  max_message_size           = 262144
-  receive_wait_time_seconds  = 0
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.payment-order-create-accept-dlq.arn
-    maxReceiveCount     = 5
-  })
-
-  depends_on = [aws_sqs_queue.payment-order-create-accept-dlq]
-}
-
 resource "aws_sqs_queue" "order-status-update-queue" {
   name                       = "order-status-update-queue"
   visibility_timeout_seconds = 30
@@ -140,4 +76,20 @@ resource "aws_sqs_queue" "order-status-update-queue" {
   })
 
   depends_on = [aws_sqs_queue.order-status-update-dlq]
+}
+
+resource "aws_sqs_queue" "cook-order-create-queue" {
+  name                       = "cook-order-create-queue"
+  visibility_timeout_seconds = 30
+  message_retention_seconds  = 86400
+  delay_seconds              = 0
+  max_message_size           = 262144
+  receive_wait_time_seconds  = 0
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.cook-order-create-dlq.arn
+    maxReceiveCount     = 5
+  })
+
+  depends_on = [aws_sqs_queue.cook-order-create-dlq]
 }
